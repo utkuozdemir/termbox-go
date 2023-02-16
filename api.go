22fa834328ec3112880a0e981d9906d08b5d5ca7
Utku Ozdemir <utku.ozdemir@siderolabs.com>
2023-02-16 22:00:22 +0100

    feat: allow specifying TTY device file
    Signed-off-by: Utku Ozdemir <utku.ozdemir@siderolabs.com>

--- api.go
+++ api.go
@@ -1,3 +1,4 @@
+//go:build !windows
 // +build !windows
 
 package termbox
@@ -19,12 +20,18 @@ import (
 // After successful initialization, the library must be finalized using 'Close' function.
 //
 // Example usage:
-//      err := termbox.Init()
-//      if err != nil {
-//              panic(err)
-//      }
-//      defer termbox.Close()
+//
+//	err := termbox.Init()
+//	if err != nil {
+//	        panic(err)
+//	}
+//	defer termbox.Close()
 func Init() error {
+	return InitTTY("/dev/tty")
+}
+
+// InitTTY initializes a specific TTY, such as /dev/tty1, /dev/tty2, etc.
+func InitTTY(ttyFilePath string) error {
 	if IsInit {
 		return nil
 	}
@@ -32,17 +39,17 @@ func Init() error {
 	var err error
 
 	if runtime.GOOS == "openbsd" || runtime.GOOS == "freebsd" {
-		out, err = os.OpenFile("/dev/tty", os.O_RDWR, 0)
+		out, err = os.OpenFile(ttyFilePath, os.O_RDWR, 0)
 		if err != nil {
 			return err
 		}
 		in = int(out.Fd())
 	} else {
-		out, err = os.OpenFile("/dev/tty", os.O_WRONLY, 0)
+		out, err = os.OpenFile(ttyFilePath, os.O_WRONLY, 0)
 		if err != nil {
 			return err
 		}
-		in, err = syscall.Open("/dev/tty", syscall.O_RDONLY, 0)
+		in, err = syscall.Open(ttyFilePath, syscall.O_RDONLY, 0)
 		if err != nil {
 			return err
 		}
@@ -498,34 +505,34 @@ func SetInputMode(mode InputMode) InputMode {
 
 // Sets the termbox output mode. Termbox has four output options:
 //
-// 1. OutputNormal => [1..8]
-//    This mode provides 8 different colors:
-//        black, red, green, yellow, blue, magenta, cyan, white
-//    Shortcut: ColorBlack, ColorRed, ...
-//    Attributes: AttrBold, AttrUnderline, AttrReverse
+//  1. OutputNormal => [1..8]
+//     This mode provides 8 different colors:
+//     black, red, green, yellow, blue, magenta, cyan, white
+//     Shortcut: ColorBlack, ColorRed, ...
+//     Attributes: AttrBold, AttrUnderline, AttrReverse
 //
-//    Example usage:
-//        SetCell(x, y, '@', ColorBlack | AttrBold, ColorRed);
+//     Example usage:
+//     SetCell(x, y, '@', ColorBlack | AttrBold, ColorRed);
 //
-// 2. Output256 => [1..256]
-//    In this mode you can leverage the 256 terminal mode:
-//    0x01 - 0x08: the 8 colors as in OutputNormal
-//    0x09 - 0x10: Color* | AttrBold
-//    0x11 - 0xe8: 216 different colors
-//    0xe9 - 0x1ff: 24 different shades of grey
+//  2. Output256 => [1..256]
+//     In this mode you can leverage the 256 terminal mode:
+//     0x01 - 0x08: the 8 colors as in OutputNormal
+//     0x09 - 0x10: Color* | AttrBold
+//     0x11 - 0xe8: 216 different colors
+//     0xe9 - 0x1ff: 24 different shades of grey
 //
-//    Example usage:
-//        SetCell(x, y, '@', 184, 240);
-//        SetCell(x, y, '@', 0xb8, 0xf0);
+//     Example usage:
+//     SetCell(x, y, '@', 184, 240);
+//     SetCell(x, y, '@', 0xb8, 0xf0);
 //
-// 3. Output216 => [1..216]
-//    This mode supports the 3rd range of the 256 mode only.
-//    But you don't need to provide an offset.
+//  3. Output216 => [1..216]
+//     This mode supports the 3rd range of the 256 mode only.
+//     But you don't need to provide an offset.
 //
-// 4. OutputGrayscale => [1..26]
-//    This mode supports the 4th range of the 256 mode
-//    and black and white colors from 3th range of the 256 mode
-//    But you don't need to provide an offset.
+//  4. OutputGrayscale => [1..26]
+//     This mode supports the 4th range of the 256 mode
+//     and black and white colors from 3th range of the 256 mode
+//     But you don't need to provide an offset.
 //
 // In all modes, 0x00 represents the default color.
 //
